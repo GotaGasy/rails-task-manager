9e518f03bdd1d1de2658ca4d7d8b794be4f10ffb
Manoa Rasolomalala <manoatony@gmail.com>
2022-05-17 17:00:56 +0200

    task apps

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
-  # Read all
+  # Create a task => get & post
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  post '/tasks', to: 'tasks#create'
 
-  # Create
+  # Update a task => get & patch
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch '/tasks/:id', to: 'tasks#update'
 
-  # Read one
+  # Read all tasks
+  get '/tasks', to: 'tasks#index', as: :index
 
-  # Update
+  # Read one task => :id / show
+  get '/tasks/:id', to: 'tasks#show', as: :task
 
-  # Delete
-
+  # Delete a task => :id
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
